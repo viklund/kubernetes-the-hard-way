13e6cb3fa9121409d2fc7cb0db3c801131de51ff
Johan Viklund <johan.viklund@nbis.se>
2019-08-07 16:05:17 +0200

    Shell script to setup everything

---
+++ work/run-all.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -xe
+
+#./bin/terraform apply --auto-approve || true
+#sleep 5
+#./bin/terraform apply --auto-approve
+
+./fix-ssh-keys.pl
+./fix-group-vars.pl
+
+ansible-playbook -i inventory playbook-00-*.yml
+
+./fix-ssh-keys.pl
+
+ansible-playbook -i inventory playbook-02-*.yml
+ansible-playbook -i inventory playbook-04-*.yml
+ansible-playbook -i inventory playbook-05-*.yml
+ansible-playbook -i inventory playbook-06-*.yml
+ansible-playbook -i inventory playbook-07-*.yml
+ansible-playbook -i inventory playbook-08-*.yml
