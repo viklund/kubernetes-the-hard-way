6c2a6682ecf7f05e494e0eefe381762bf62960cb
Johan Viklund <johan.viklund@nbis.se>
2019-08-09 12:26:48 +0200

    Various updates

--- work/run-all.sh
+++ work/run-all.sh
@@ -4,20 +4,25 @@ set -xe
 
 #./bin/terraform apply --auto-approve || true
 #sleep 5
-#./bin/terraform apply --auto-approve
-#sleep 20
+./bin/terraform apply --auto-approve
+sleep 60
 
 ./fix-ssh-keys.pl
 ./fix-group-vars.pl
 
-ansible-playbook -i inventory playbook-00-*.yml
+#ansible-playbook -i inventory playbook-00-*.yml ||  true
+#sleep 5
+ansible-playbook -i inventory playbook-00-*.yml; sleep 3
 
 ./fix-ssh-keys.pl
 
-ansible-playbook -i inventory playbook-02-*.yml
-ansible-playbook -i inventory playbook-04-*.yml
-ansible-playbook -i inventory playbook-05-*.yml
-ansible-playbook -i inventory playbook-06-*.yml
-ansible-playbook -i inventory playbook-07-*.yml
-ansible-playbook -i inventory playbook-08-*.yml
-ansible-playbook -i inventory playbook-09-*.yml
+ansible-playbook -i inventory playbook-02-*.yml; sleep 3
+ansible-playbook -i inventory playbook-04-*.yml; sleep 3
+ansible-playbook -i inventory playbook-05-*.yml; sleep 3
+ansible-playbook -i inventory playbook-06-*.yml; sleep 3
+ansible-playbook -i inventory playbook-07-*.yml; sleep 3
+ansible-playbook -i inventory playbook-08-*.yml; sleep 3
+ansible-playbook -i inventory playbook-09-*.yml; sleep 3
+ansible-playbook -i inventory playbook-10-*.yml; sleep 3
+ansible-playbook -i inventory playbook-11-*.yml; sleep 3
+ansible-playbook -i inventory playbook-12-*.yml; sleep 3
